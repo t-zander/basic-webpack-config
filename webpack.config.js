72bd7c87bb9eed6c0ae0588f08bd3bf6befa764e
t-zander <alex.sergeevich.tarasenko@gmail.com>
2020-03-08 17:48:51 +0200

    updated webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,11 @@
-// This is a config file that webpack will use
+/* This is a config file that webpack will use */
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  // where to put bundled js file
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
